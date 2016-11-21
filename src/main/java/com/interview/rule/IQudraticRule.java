5d5b45d3545d22c2a37d729463aa4a8b2136bc36
VyacheslavRarata <cayneir@gmail.com>
2016-11-21 22:46:59 +0200

    #8 added IQuadraticRule interface

---
+++ src/main/java/com/interview/rule/IQudraticRule.java
@@ -0,0 +1,9 @@
+package com.interview.rule;
+
+import com.interview.bean.CoefficientBean;
+import com.interview.domains.Equations;
+
+public interface IQudraticRule {
+
+	Equations getResult(CoefficientBean coefficientBean);
+}
