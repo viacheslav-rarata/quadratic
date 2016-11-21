230ff24bb363b7b112229a350dbeb04b88fed7a2
VyacheslavRarata <cayneir@gmail.com>
2016-11-21 23:09:37 +0200

    #8 changed return type to CoefficientBean

--- src/main/java/com/interview/rule/IQudraticRule.java
+++ src/main/java/com/interview/rule/IQudraticRule.java
@@ -1,9 +1,8 @@
 package com.interview.rule;
 
 import com.interview.bean.CoefficientBean;
-import com.interview.domains.Equations;
 
 public interface IQudraticRule {
 
-	Equations getResult(CoefficientBean coefficientBean);
+	CoefficientBean getResult(CoefficientBean coefficientBean);
 }
