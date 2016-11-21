d634712774596c4e2f6cff1eae19f51d56b61816
VyacheslavRarata <cayneir@gmail.com>
2016-11-21 21:50:47 +0200

    #10 added CoefficientBean

---
+++ src/main/java/com/interview/bean/CoefficientBean.java
@@ -0,0 +1,60 @@
+package com.interview.bean;
+
+public class CoefficientBean {
+
+	private String leadingCoefficien;
+	private String secondCoefficien;
+	private String freeMember;
+	private Double discriminant;
+	private Double firstVariable;
+	private Double secondVariable;
+
+	public String getLeadingCoefficien() {
+		return leadingCoefficien;
+	}
+
+	public void setLeadingCoefficien(String leadingCoefficien) {
+		this.leadingCoefficien = leadingCoefficien;
+	}
+
+	public String getSecondCoefficien() {
+		return secondCoefficien;
+	}
+
+	public void setSecondCoefficien(String secondCoefficien) {
+		this.secondCoefficien = secondCoefficien;
+	}
+
+	public String getFreeMember() {
+		return freeMember;
+	}
+
+	public void setFreeMember(String freeMember) {
+		this.freeMember = freeMember;
+	}
+
+	public Double getDiscriminant() {
+		return discriminant;
+	}
+
+	public void setDiscriminant(Double discriminant) {
+		this.discriminant = discriminant;
+	}
+
+	public Double getFirstVariable() {
+		return firstVariable;
+	}
+
+	public void setFirstVariable(Double firstVariable) {
+		this.firstVariable = firstVariable;
+	}
+
+	public Double getSecondVariable() {
+		return secondVariable;
+	}
+
+	public void setSecondVariable(Double secondVariable) {
+		this.secondVariable = secondVariable;
+	}
+
+}
